4fbbbfce99aec92f075181c9bd0cc7cf8c742155
Danijel Hodzic <danijel.hodzic@rs.ey.com>
2023-06-16 10:37:15 +0200

    initial commit - project init

---
+++ nssmart/src/main/java/com/ey/hackaton/nssmart/NssmartApplication.java
@@ -0,0 +1,13 @@
+package com.ey.hackaton.nssmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NssmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NssmartApplication.class, args);
+	}
+
+}
---
+++ nssmart/src/test/java/com/ey/hackaton/nssmart/NssmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ey.hackaton.nssmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NssmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
